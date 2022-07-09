12244ea0efb553c4ede5cf22f18daf405e400eb3
Rick Dymond <rdymond@oakland.edu>
2022-07-09 01:25:18 -0400

    added front-end to database infrastructure.

--- Index.js
+++ Index.js
@@ -1,28 +1,206 @@
+//variables for server
 const path = require("path");
 const ejs = require("ejs");
+const mysql = require("mysql");
+const env = require('dotenv');
+const cors = require('cors');
+const fileUpload = require('express-fileUpload');
 const session = require("express-session");
 const express = require('express');
 const { userInfo } = require("os");
 const { response } = require("express");
+const { strictEqual } = require("assert");
+const { clearScreenDown } = require("readline");
+const { brotliDecompress } = require("zlib");
+const { writer } = require("repl");
 const app = express();
+env.config();
 
+//database connection object
+const database = mysql.createConnection({
+      //database
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'music_db'
+});
+
+//database connection function
+database.connect(function(err) {
+  if(err) {
+    console.log('Error no connection');
+    throw err;
+  }
+  console.log('Connected to Database');
+});
+
+//middleware & routers
 app.use(express.static(path.join(__dirname, "public")));
-app.set('view engine', 'index');
+
+app.set('view engine', 'ejs');
 
 app.use(express.json());
+
 app.use(express.urlencoded({ 
   extended: true, 
   SameSite: 'none'
 }));
 
-app.get("/", (req, res) => {
-    res.render('index');
+app.use(cors());
+
+app.use(fileUpload());
+
+
+//test server via http
+app.get("/home", (req, res) => {
+   res.render(__dirname + '/views/index.ejs', {test: 'HELLO!'})
+   console.log('HTTP reponse successful');
   });
 
-app.get("/public/index.html", (req, res) => {
-    res.render('index');
+  //upload mp3 file
+ app.get("/upload", (req, res) => {
+  res.redirect('/home');
+  console.log('Upload Reponse Successful');
   });
 
+app.post('/home', function(req, res){
+  let mp3_data = req.body;
+  let mp3_file = req.files.mp3_file;
+
+  mp3_file.mv('/public/audio/'+ mp3_file.name, function (error) {
+    if(error) {
+    console.log('MP3 Upload not succesful');
+    console.log(error);
+  }else{
+    console.log('MP3 Upload Succesful');
+  }
+  console.log(mp3_file);
+  });
+});
+
+//create db for admin
+app.get('/create_db', (req, res) => {
+  let databaseName = 'music_db';
+   sql = 'CREATE DATABASE music_db';
+   database.query(sql, (err, result) => {
+   if(err) throw err;
+   console.log(result);
+   console.log('Database music_db Created');
+   res.redirect('/home');
+   
+   });
+   
+//drop db for admin
+app.get('/drop_db', (req, res) => {
+  let databaseName = 'music_db';
+    sql = 'DROP DATABASE music_db';
+    database.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    //res.sendFile(__dirname + '/public/index.html');
+    res.redirect('/home');
+    });
+  });
+});
+
+//create table for admin
+app.get('/create_table', (req, res) => {
+    let sql = 'CREATE TABLE music_table(id INT AUTO_INCREMENT PRIMARY KEY, song VARCHAR(255), artist VARCHAR(255), uploader VARCHAR(255), mp3 BLOB)';
+    let databaseName = 'music_db';
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    res.redirect('/home');
+    console.log('Table Created');
+  });
+
+//drop table from database for admin
+  app.get('/drop_table', (req, res) => {
+    let sql = 'DROP TABLE music_table';
+    let databaseName = 'music_db';
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    res.redirect('/home');
+    console.log('Table Deleted');
+  });
+
+  //database querys
+  //create
+  app.get('/insert_table', (request, response) => {
+    let sql = "INSERT INTO music_table (id, song, artist, uploader, mp3) VALUES (NULL, 'Ripple', 'Grateful Dead', 'Rick Dymond', 'file')";
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    response.redirect('/home');
+    console.log('Insert Query');
+  });
+
+  //test create
+  app.post('/test_create', (request, response) => {
+
+  });
+
+  //search
+  app.get('/search_table', (request, response) => {
+    let sql = 'SELECT * FROM music_table';
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    response.redirect('/home');
+    console.log('Search Querry');
+  });
+
+  //search 2
+  app.get('/search', (req, res) => {
+  console.log(req.query);
+  console.log(res.query);
+  let sql = 'SELECT * FROM music_table';
+  database.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+  res.json({
+    message: 'search JSON reponse successful',
+    id: req.query.id,
+    song: req.query.song,
+    artist: req.query.artist,
+    uploader: req.query.uploader,
+    mp3_file: req.query.mp3_file,
+  });
+  //http://localhost:3000/search?id=1&song=Ripple&artist=Grateful%20Dead&uploader=Rick%20Dymond&mp3_file=BLOB
+  //res.redirect('/');
+
+  //drop row
+  app.get('/drop_row', (req, res) => {
+    let sql = 'DELETE FROM music_table WHERE id=1';
+    let databaseName = 'music_db';
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    res.redirect('/home');
+    console.log('Row Deleted');
+  });
+
+  });
+
+  app.get('/drop_row', (req, res) => {
+    let sql = 'DELETE FROM music_table WHERE id=1';
+    let databaseName = 'music_db';
+    database.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    res.redirect('/home');
+    console.log('Row Deleted');
+  });
+
+//port for application listening
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,5 @@
+//const { response } = require("express");
+
 //variables
 let uploader = document.getElementById('file');
 
@@ -19,15 +21,40 @@ let updateBTN = document.getElementById('update');
 
 let deleteBTN = document.getElementById('delete');
 
+let songField = document.getElementById('song');
+
+let artistField = document.getElementById('artist_name');
+
+let albumField =document.getElementById('album');
+
+let uploaderField = document.getElementById('uploader_name');
+
 file.onchange = () => {
+    let sourceTag = 'source';
     const selectedFile = file.files[0];
     console.log(selectedFile);
+    loadPlayer = document.getElementById('audio_player');
+    mp3_src = document.getElementById('mp3_src');
+    mp3_src.setAttribute('src', './audio/' + selectedFile.name);
+    //location.reload(true);
+  
+    console.log(document.getElementById('source'));
   }
 
   const handler = () => {
     const selectedFiles = [...file.files];
     console.log(file);
   }
+
+//button disabler
+ uploadBTN.disabled = true;
+ searchBTN.disabled = true;
+ updateBTN.disabled = true;
+ deleteBTN.disabled = true;
+
+
+
+  
 
 //event listeners
 uploadBTN.addEventListener('click', upload); {
@@ -46,41 +73,139 @@ deleteBTN.addEventListener('click', del); {
     del();
 }
 
-uploader.addEventListener('change', handler)
+uploader.addEventListener('change', handler); {
+    upload();
+}
+
+songField.addEventListener('input', checkFields); {
+    if (song.value != "") {
+    checkFields(song);
+    UploadcheckFields(song);
+    }
+}
+
+songField.addEventListener('input', UploadcheckFields); {
+    if (song.value != "") {
+    UploadcheckFields(song);
+    }
+}
+
+artistField.addEventListener('input', checkFields); {
+    if (artist.value != "") {
+    checkFields(artist);
+    }
+}
+
+artistField.addEventListener('input', UploadcheckFields); {
+    if (song.value != "") {
+    UploadcheckFields(artist);
+    }
+}
+
+albumField.addEventListener('input', checkFields); {
+    if (album.value != "") {
+    checkFields(album);
+    }
+}
+
+albumField.addEventListener('input', UploadcheckFields); {
+    if (album.value != "") {
+    UploadcheckFields(album);
+    }
+}
+
+uploaderField.addEventListener('input', checkFields); {
+    if (uploader_name.value != "") {
+    checkFields(uploader_name);
+    UploadcheckFields(uploader_name);
+    }
+}
+
+uploaderField.addEventListener('input', UploadcheckFields); {
+    if (uploader_name.value != "") {
+    UploadcheckFields(uploader_name);
+    }
+}
+
+// load database table
+//document.addEventListener('DOMContentLoaded', funtion () {
+  //fetch('http://localhost:3000/search')
+  //.then( response => response.json())
+  //.then(data => console.log(data));
+  //loadTable([]);
+//});
+
+//
+//let checkField = checkFields();
+
+function checkFields (ev) {
+    this.song;
+    this.album;
+    this.artist;
+    this.uploader_name;
+    if (song.value && album.value && artist.value && uploader_name.value != "") {
+        searchBTN.disabled = false;
+        updateBTN.disabled = false;
+        deleteBTN.disabled = false;
+        console.log('Buttons enabled');
+    }
+};
+
+function UploadcheckFields (ev) {
+    this.song;
+    this.album;
+    this.artist;
+    this.uploader_name;
+    this.file;
+    if (song.value && album.value && artist.value && uploader_name.value && file.onchange(true) != "") {
+        uploadBTN.disabled = false;
+        console.log('Upload Button enabled');
+    }
+};
+
+//load database data into index.html
+function loadTable(data) {
+    const table = document.querySelector('table tbody');
+    let tableHtml = "";
+  
+    if(data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='3'>No Data</td></tr>";
+    }
+  }
+
 
 //functions
 function upload(ev) {
     let uploadGood = document.createElement("p")
     uploadGood.id = "uploadGood"
     uploadGood.innerHTML = ""
-    
-    if (song.value && album.value && artist.value && uploader_name.value && uploader.value != "") {
-    uploadGood.innerHTML = "Succesful Upload!"
-    playerContainer.appendChild(uploadGood);
-    song.value = "";
-    album.value = "";
-    artist.value = "";
-    uploader_name.value = "";
-    console.log('Upload Succesful');
+    if (song.value && album.value && artist.value && uploader_name.value != "") {
+        disableButton = false;
+        uploadGood.innerHTML = "Succesful Upload!"
+        playerContainer.appendChild(uploadGood);
+        song.value = "";
+        album.value = "";
+        artist.value = "";
+        uploader_name.value = "";
+        console.log('Upload Succesful');
     }
 }
-
 function search(ev) {
     let searchGood = document.createElement("p")
     searchGood.id = "searchGood"
     searchGood.innerHTML = ""
-    
-    if (song.value || album.value || artist.value || uploader_name.value != "") {
-    searchGood.innerHTML = "Succesful Search!"
-    playerContainer.appendChild(searchGood);
-    song.value = "";
-    album.value = "";
-    artist.value = "";
-    uploader_name.value = "";
-    console.log('Search Succesful');
+        if (song.value || album.value || artist.value || uploader_name.value != "") {
+           // location.reload();
+            disableButton = false;
+            searchGood.innerHTML = "Succesful Search!"
+            playerContainer.appendChild(searchGood);
+            song.value = "";
+            album.value = "";
+            artist.value = "";
+            uploader_name.value = "";
+            console.log('Search Succesful');
     }
 }
-
 function update(ev) {
     let updateGood = document.createElement("p")
     updateGood.id = "updateGood"
@@ -96,7 +221,6 @@ function update(ev) {
     console.log('Update Succesful');
     }
 }
-
 function del(ev) {
     let delGood = document.createElement("p")
     delGood.id = "delGood"
